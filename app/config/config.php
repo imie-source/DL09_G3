94b0d62cb51d0111541cfdb39d429c0306ee1f4b
Kevin BALINI <kevin.balini@gmail.com>
2014-03-07 23:45:11 +0100

    Update config file

--- app/config/config.php
+++ app/config/config.php
@@ -1,13 +1,6 @@
 <?php
 
 return new \Phalcon\Config(array(
-    'database' => array(
-        'adapter'     => 'Mysql',
-        'host'        => 'localhost',
-        'username'    => 'root',
-        'password'    => 'root',
-        'dbname'      => 'nannyster',
-    ),
     'application' => array(
         'controllersDir' => APP_DIR . '/controllers/',
         'modelsDir'      => APP_DIR . '/models/',
@@ -23,13 +16,13 @@ return new \Phalcon\Config(array(
     ),
     'mail' => array(
         'fromName' => 'SPASM',
-        'fromEmail' => 'no-reply@spsm.fr',
+        'fromEmail' => 'no-reply@spasm.fr',
         'smtp' => array(
             'server' => 'smtp.gmail.com',
             'port' => 465, //587
             'security' => 'ssl',
-            'username' => 'kevin.balini@gmail.com',
-            'password' => 'MP7REccg'
+            'username' => '',
+            'password' => ''
         )
     ),
     'amazon' => array(
