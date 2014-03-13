71cf0ca780db1477541201ebf78cd9d23bb62b25
Simon Louvet <romain.bongibault@orange.fr>
2014-03-13 16:57:56 +0100

    project 16h57

--- app/controllers/ProjectsController.php
+++ app/controllers/ProjectsController.php
@@ -5,8 +5,26 @@ namespace Nannyster\Controllers;
 class ProjectsController extends ControllerBase
 {
 
-	public function indexAction()
-    {
+	public function manageAction(){
+
+    $this->tag->prependTitle('Manager d\'utilisateurs - ');
+    $user = $this->auth->getUser();
+    $this->view->setVar('breadcrumbs', array(
+            'Administration' => array(
+                'controller' => 'administration',
+                'action' => 'index'),
+            'gestion des utilisateurs' => array(
+                'last' => true)
+        ));
+    $this->assets->addJs('js/jquery.dataTables.min.js');
+    $this->assets->addJs('js/jquery.dataTables.bootstrap.js');
+    $this->assets->addJs('js/bootbox.min.js');
+    $this->assets->addJs('js/users/manage.js');
+
+    $this->view->setVar('users', Users::find());
+    $this->view->setVar('profiles', Profiles::find());
+    $this->view->setVar('promotions', Promotions::find());
+
     
     }
 }
