acdd618175f347fb2cc991f69f31591a84f67273
Simon Louvet <dali.sabri@hotmail.fr>
2014-03-25 10:11:19 +0100

    dump

--- public/js/users/import.js
+++ public/js/users/import.js
@@ -1,24 +1,11 @@
 $(document).ready(function() {
-	$('#id-input-file-2').ace_file_input({
-		no_file:'Aucun fichier choisi ...',
-		btn_choose:'Choisir',
-		btn_change:'Changer',
-		droppable:true,
-		onchange:null,
-		thumbnail:false //| true | large
-		//whitelist:'gif|png|jpg|jpeg'
-		//blacklist:'exe|php'
-		//onchange:''
-		//
-	});
-	
 	$('#id-input-file-3').ace_file_input({
 		style:'well',
-		btn_choose:'Glissez votre fichier ici ou cliquez pour le choisir',
+		btn_choose:'Glissez votre fichier csv ici ou cliquez pour le choisir',
 		btn_change:null,
 		no_icon:'icon-cloud-upload',
 		droppable:true,
-		thumbnail:'small'//large | fit
+		thumbnail:'large'//large | fit
 		//,icon_remove:null//set null, to hide remove/reset button
 		/**,before_change:function(files, dropped) {
 			//Check an example below
